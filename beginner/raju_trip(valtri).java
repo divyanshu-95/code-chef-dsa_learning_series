cf85465fac653e2d0ce06b95e59b456a7be72dbb
Divyanshu Varshney <51895957+divyanshu-95@users.noreply.github.com>
2021-06-30 23:03:17 +0530

    Create raju_trip(valtri).java

---
+++ beginner/raju_trip(valtri).java
@@ -0,0 +1,24 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class CodeChef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    try{
+		    Scanner s=new Scanner(System.in);
+		    int n=s.nextInt();//testcase
+		    if(n%5==0 || n%6==0){
+		        System.out.println("YES");
+		    }else{
+		        System.out.println("NO");
+		    }
+		}catch(Exception e){
+	        
+	    }
+	}
+}
