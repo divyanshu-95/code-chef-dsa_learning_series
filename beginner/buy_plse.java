4f0936ea94ce088b5c8c026c7226aca301a54b80
Divyanshu Varshney <51895957+divyanshu-95@users.noreply.github.com>
2021-06-30 21:39:10 +0530

    Create buy_plse.java

---
+++ beginner/buy_plse.java
@@ -0,0 +1,23 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class CodeChef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    try{
+		    Scanner s=new Scanner(System.in);
+		    int a=s.nextInt();
+		    int b=s.nextInt();
+		    int c=s.nextInt();
+		    int d=s.nextInt();
+		    System.out.println(a*c+b*d);
+		}catch(Exception e){
+	        
+	    }
+	}
+}
