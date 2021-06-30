32cc76e6ea367cafd02a2df7aa805be55b2a8424
Divyanshu Varshney <51895957+divyanshu-95@users.noreply.github.com>
2021-06-30 23:00:28 +0530

    Create rngeodd.java

---
+++ beginner/rngeodd.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class CodeChef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    try{
+		    Scanner s=new Scanner(System.in);
+		    int a=s.nextInt();
+		    int b=s.nextInt();
+		    for(int i=a;i<=b;i++){
+		        if(i%2!=0){
+		            System.out.print(i+" ");
+		        }
+		    }
+		    
+		}catch(Exception e){
+	        
+	    }
+	}
+}
