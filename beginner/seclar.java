1f399ef1e23636a575373d91127c70f69024c621
Divyanshu Varshney <51895957+divyanshu-95@users.noreply.github.com>
2021-06-30 22:57:18 +0530

    Create seclar.java

---
+++ beginner/seclar.java
@@ -0,0 +1,22 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class CodeChef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    try{
+		    Scanner s=new Scanner(System.in);
+		    int a[]={s.nextInt(),s.nextInt(),s.nextInt()};
+		    Arrays.sort(a);
+		    System.out.println(a[3-2]);
+		    
+		}catch(Exception e){
+	        
+	    }
+	}
+}
