67a5ce3d222b0c76298fa3e9cdaf35878942c949
Divyanshu Varshney <51895957+divyanshu-95@users.noreply.github.com>
2021-07-01 11:36:13 +0530

    Create addnatrl.java

---
+++ beginner/addnatrl.java
@@ -0,0 +1,22 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    try{
+		    Scanner s=new Scanner(System.in);
+		  //  int count=0;
+		    long n=s.nextInt();
+		    long sum=(n*(n+1)/2);
+		    System.out.println(sum);
+		}catch(Exception e){
+	        
+	    }
+	}
+}
